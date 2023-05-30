b793eca615948ba5fa7274e27de208ed7a4a884a
Yofandra <drayofan@gmail.com>
2023-05-30 15:09:13 +0700

    praktikum 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table='users';
     protected $fillable = [
+        'username',
         'name',
         'email',
         'password',
